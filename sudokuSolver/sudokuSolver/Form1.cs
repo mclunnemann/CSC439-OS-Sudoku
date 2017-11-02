05d6825f8350c45f095d83f629a4d10a76edde04
Michael Lunnemann <lunnemm@gmail.com>
2017-11-02 19:08:03 -0400

    implemented load and solve methods and changed gui

--- sudokuSolver/sudokuSolver/Form1.cs
+++ sudokuSolver/sudokuSolver/Form1.cs
@@ -7,109 +7,197 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Timers;
 
-namespace sudokuSolver {
-	public partial class Form1 : Form {
-        public static bool easy, medium, hard;
+namespace sudokuSolver
+{
 
-        string puzzle, answer;
-		const int cColWidth = 45;
-		const int cRowHeigth = 45;
-		const int cMaxCell = 9;
-		const int cSidelength = cColWidth * cMaxCell + 3;
-		bool check = false;
-		DataGridView DGV;
-		Sudoku sudoku = new Sudoku();
+    public partial class Form1 : Form
+    {
+        public static bool easy;
+        public static bool medium;
+        public static bool hard;
 
-		public Form1() {
-			InitializeComponent();
-		}
+        string puzzle = "", answer = "";
+        const int cColWidth = 45;
+        const int cRowHeigth = 45;
+        const int cMaxCell = 9;
+        const int cSidelength = cColWidth * cMaxCell + 3;
+        DataGridView DGV;
+        Sudoku sudoku = new Sudoku();
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-		private void Form1_Load(object sender, EventArgs e) {
-			setUpBoard();
-		}
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inputText.Visible = true;
+            inputText.Enabled = false;
+            btn_import.Visible = true;
+            btn_import.Enabled = false;
+            btn_Solve.Visible = true;
+            btn_Solve.Enabled = false;
+            easyToolStripMenuItem.Checked = true;
+            easy = true;
+            setUpBoard();
+        }
 
 
-		private void newGame() {
-			resetBoard();
-			fillGrid();
-		}
+        private void newGame()
+        {
+            resetBoard();
+            fillGrid();
+        }
 
-		public void setUpGrid(string grid) {
-			foreach (var c in grid) {
-				int num = c - '0';
+        public void setUpGrid(string grid)
+        {
+            foreach (var c in grid)
+            {
+                int num = c - '0';
 
-			}
-		}
-		private void fillGrid() {
-			puzzle = "";
-			answer = "";
-			sudoku.Randomize(out answer, out puzzle);
-			label1.Text = puzzle;
-			label2.Text = answer;
-			int letter = 0;
-			char[] letters = new char[puzzle.Length];
-			for (int i = 0; i < letters.Length; i++) {
-				letters[i] = puzzle[i];
-			}
-				foreach (DataGridViewRow i in DGV.Rows) {
-					foreach (DataGridViewCell j in i.Cells) {
-					if (letters[letter] == '0') {
-						letters[letter] = ' ';
-					} else {
-						j.Value = letters[letter];
-						j.Style.BackColor = Color.LightGray;
-						j.ReadOnly = true;
-					}
-					letter++;
-				}
-			}
-		}
-		private void setUpBoard() {
-			DGV = new DataGridView();
-			DGV.Name = "DGV";
-			DGV.AllowUserToResizeColumns = false;
-			DGV.AllowUserToResizeRows = false;
-			DGV.AllowUserToAddRows = false;
-			DGV.RowHeadersVisible = false;
-			DGV.ColumnHeadersVisible = false;
-			DGV.GridColor = Color.DarkBlue;
-			DGV.DefaultCellStyle.BackColor = Color.AliceBlue;
-			DGV.ScrollBars = ScrollBars.None;
-			DGV.Size = new Size(cSidelength, cSidelength);
-			DGV.Location = new Point(50, 50);
-			DGV.Font = new System.Drawing.Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-			DGV.ForeColor = Color.DarkBlue;
-			DGV.SelectionMode = DataGridViewSelectionMode.CellSelect;
-			// add 81 cells
-			for (int i = 0; i < cMaxCell; i++) {
-				DataGridViewTextBoxColumn TxCol = new DataGridViewTextBoxColumn();
-				TxCol.MaxInputLength = 1;   // only one digit allowed in a cell
-				DGV.Columns.Add(TxCol);
-				DGV.Columns[i].Name = "Col " + (i + 1).ToString();
-				DGV.Columns[i].Width = cColWidth;
-				DGV.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+        }
+        private void fillGrid()
+        {
+            try
+            {
+                if (btn_import.Enabled == true)
+                {   if(inputText.Text != "")
+                    {
+                        puzzle = inputText.Text;
+                    }                    
+                    answer = sudoku.solver(puzzle);
+                }
+                else
+                {
+                    sudoku.Randomize(out answer, out puzzle);
+                }
+                label1.Text = puzzle;
+                label2.Text = answer;
+                int letter = 0;
+                char[] letters = new char[puzzle.Length];
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    letters[i] = puzzle[i];
+                }
+                foreach (DataGridViewRow i in DGV.Rows)
+                {
+                    foreach (DataGridViewCell j in i.Cells)
+                    {
+                        if (letters[letter] == '0')
+                        {
+                            letters[letter] = ' ';
+                        }
+                        else
+                        {
+                            j.Value = letters[letter];
+                            j.Style.BackColor = Color.LightGray;
+                            j.ReadOnly = true;
+                        }
+                        letter++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                resetBoard();
+                MessageBox.Show("Invalid Sudoku Board Input. Please Enter a valid sudoku Board.");
+            }
+        }
 
-				DataGridViewRow row = new DataGridViewRow();
-				row.Height = cRowHeigth;
-				DGV.Rows.Add(row);
-			}
-			// mark the 9 square areas consisting of 9 cells
-			DGV.Columns[2].DividerWidth = 2;
-			DGV.Columns[5].DividerWidth = 2;
-			DGV.Rows[2].DividerHeight = 2;
-			DGV.Rows[5].DividerHeight = 2;
-			Controls.Add(DGV);
-		}
+        private async void fillSolvedGrid(string puzzle)
+        {
+            int letter = 0;
+            char[] letters = new char[puzzle.Length];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = puzzle[i];
+            }
+            foreach (DataGridViewRow i in DGV.Rows)
+            {
+                foreach (DataGridViewCell j in i.Cells)
+                {
+                    if (letters[letter] == '0')
+                    {
+                        letters[letter] = ' ';
+                    }
+                    else
+                    {
+                        j.Value = letters[letter];
+                        j.Style.BackColor = Color.LightGray;
+                        j.ReadOnly = true;
+                    }
+                    await Task.Delay(200);
+                    letter++;
+                }
+            }
+            MessageBox.Show("Problem Solved!");
+            btn_Solve.Enabled = false;
+            btn_import.Enabled = false;
+            inputText.Clear();
+            inputText.Enabled = false;
+        }
 
-		private void newGameToolStripMenuItem_Click(object sender, EventArgs e) {
-			newGame();
-		}
+        private void setUpBoard()
+        {
+            DGV = new DataGridView();
+            DGV.Name = "DGV";
+            DGV.AllowUserToResizeColumns = false;
+            DGV.AllowUserToResizeRows = false;
+            DGV.AllowUserToAddRows = false;
+            DGV.RowHeadersVisible = false;
+            DGV.ColumnHeadersVisible = false;
+            DGV.GridColor = Color.DarkBlue;
+            DGV.DefaultCellStyle.BackColor = Color.AliceBlue;
+            DGV.ScrollBars = ScrollBars.None;
+            DGV.Size = new Size(cSidelength, cSidelength);
+            DGV.Location = new Point(50, 50);
+            DGV.Font = new System.Drawing.Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
+            DGV.ForeColor = Color.DarkBlue;
+            DGV.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            // add 81 cells
+            for (int i = 0; i < cMaxCell; i++)
+            {
+                DataGridViewTextBoxColumn TxCol = new DataGridViewTextBoxColumn();
+                TxCol.MaxInputLength = 1;   // only one digit allowed in a cell
+                DGV.Columns.Add(TxCol);
+                DGV.Columns[i].Name = "Col " + (i + 1).ToString();
+                DGV.Columns[i].Width = cColWidth;
+                DGV.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
 
-		private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
-			Application.Exit();
-		}
+                DataGridViewRow row = new DataGridViewRow();
+                row.Height = cRowHeigth;
+                DGV.Rows.Add(row);
+            }
+            // mark the 9 square areas consisting of 9 cells
+            DGV.Columns[2].DividerWidth = 2;
+            DGV.Columns[5].DividerWidth = 2;
+            DGV.Rows[2].DividerHeight = 2;
+            DGV.Rows[5].DividerHeight = 2;
+            Controls.Add(DGV);
+        }
 
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!easy && !medium && !hard)
+            {
+                MessageBox.Show("Please Select a difficulty.");
+            }
+            else
+            {
+                btn_Solve.Enabled = true;
+                inputText.Text = "";
+
+                newGame();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
         public void easyToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (mediumToolStripMenuItem.Checked == true)
@@ -191,13 +279,59 @@ namespace sudokuSolver {
             }
         }
 
-        private void loadToolStripMenuItem_Click(object sender, EventArgs e) {
-			MessageBox.Show(this.inputText.Text);
-		}
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+            inputText.Visible = true;
+            inputText.Enabled = true;
+            btn_import.Visible = true;
+            btn_import.Enabled = true;
+            btn_Solve.Enabled = true;
+            resetBoard();
+            string inputSudoku = inputText.Text;
+        }
 
-		private void resetBoard() {
-			Controls.Remove(DGV);
-			setUpBoard();
-		}
-	}
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            if (inputText.Text == "")
+            {
+                foreach (DataGridViewRow i in DGV.Rows)
+                {
+                    foreach (DataGridViewCell j in i.Cells)
+                    {
+                        if (j.Value == null)
+                        {
+                            puzzle += '0';
+                        }
+                        puzzle += j.Value;
+                    }
+                }
+                fillGrid();
+                answer = sudoku.solver(puzzle);
+                //fillSolvedGrid(answer);
+            }
+            else
+            {
+                resetBoard();
+                fillGrid();
+            }           
+        }
+
+        private void btn_Solve_Click(object sender, EventArgs e)
+        {            
+            fillSolvedGrid(answer);           
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void resetBoard()
+        {
+            Controls.Remove(DGV);
+            setUpBoard();
+        }
+    }
 }
